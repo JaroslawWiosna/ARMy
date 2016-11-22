ea3e15f897af2a324e6575440396062f662413a3
Wojciech Urbański <wojciech_urbanski@yahoo.com>
2016-11-22 12:27:17 +0100

    UI - dodanie metody inicjalizującej

--- PythonUi/Main.py
+++ PythonUi/Main.py
@@ -6,15 +6,18 @@ from PyQt4.QtGui import QMainWindow
 from PythonUi import Initializer
 from PythonUi import wersja1
 
+def initForm():
+    init = Initializer.Initializer(ui)
+    init.initFormats()
+    init.initCanalsNumber()
+
 app = QApplication(sys.argv)
 window = QMainWindow()
 ui = wersja1.Ui_MainWindow()
 
 ui.setupUi(window)
-
-init = Initializer.Initializer(ui)
-init.initFormats()
-init.initCanalsNumber()
+initForm()
 
 window.show()
+
 sys.exit(app.exec_())
